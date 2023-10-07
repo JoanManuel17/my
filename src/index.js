1b6d948fd20bf4023557d66e7c709072001a7a25
JoanManuel17 <110619713+JoanManuel17@users.noreply.github.com>
2023-10-07 18:18:49 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import FirstApp3 from './FirstApp3';
-import FirstApp2 from './FirstApp2';
-import { ComponentApp } from './ComponentApp';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import FirstApp3 from "./FirstApp3";
+import FirstApp2 from "./FirstApp2";
+import { ComponentApp } from "./ComponentApp";
+import { BrowserRouter } from "react-router-dom";
+import { MainApp } from "./pages/MainApp";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <BrowserRouter>
+    <MainApp />
+  </BrowserRouter>,
   <React.StrictMode>
     <ComponentApp />
-    <FirstApp3 p = {10} />
-    <FirstApp2 title = "My First App" />
-     <App /> 
+    <FirstApp3 p={10} />
+    <FirstApp2 title="My First App" />
+    <App />
   </React.StrictMode>
 );
-
