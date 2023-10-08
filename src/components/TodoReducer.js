41287ef3aef44f5df00efcef4326729d546679b4
JoanManuel17 <110619713+JoanManuel17@users.noreply.github.com>
2023-10-07 19:54:53 -0500

    Cambios

--- src/components/TodoReducer.js
+++
@@ -1,8 +0,0 @@
-export const TodoReducer = (inicialState = [], action) => {
-  switch (action.type) {
-    case 'TODO ADD':
-      return [...inicialState, action.payload];
-    default:
-      return inicialState;
-  }
-};
