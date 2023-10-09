b99d52032496ae61de52e3d2dfaf28c88a7e3f85
JoanManuel17 <110619713+JoanManuel17@users.noreply.github.com>
2023-10-08 21:45:36 -0500

    Challenge07

---
+++ src/hooks/UseFetch.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/GetGifs";
+
+export const useFetch = (url) => {
+
+    const [state, setState] = useState({
+        data: null,
+        loading: true,
+        error: null
+    });
+
+    const getFecth = async () => {
+        const api = await fetch(url);
+        const data = await api.json();
+
+        setState({
+            data: data,
+            loading: false,
+            error: null
+        });
+    }
+
+    useEffect(() => {
+        getFecth();
+    }, [url]);
+
+    return {...state}
+}
