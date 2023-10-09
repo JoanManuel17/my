f633320c139c07ce2c66c5ae92058d7c87c2109e
JoanManuel17 <110619713+JoanManuel17@users.noreply.github.com>
2023-10-08 20:34:40 -0500

    Challenge06

--- src/ComponentApp.js
+++ src/ComponentApp.js
@@ -1,5 +1,6 @@
-const categories = ["first category"];
 import { useCounter } from "./hooks/UseCounter";
+
+const categories = ["first category"];
 
 export const ComponentApp = () => {
   const { count, increment, decrement, reset } = useCounter(0);
@@ -7,12 +8,11 @@ export const ComponentApp = () => {
   return (
     <>
       <h1>ComponentApp</h1>
+      <p>Counter: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
       <ol>
-        <p>Counter: {count}</p>
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
-        <button onClick={reset}>Reset</button>
-
         {categories.map((category, key) => {
           return <li key={key}>{category}</li>;
         })}
