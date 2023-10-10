d5fd45e05bd9b1a7c10ea2bfd579e0653fd63b8f
JoanManuel17 <110619713+JoanManuel17@users.noreply.github.com>
2023-10-10 14:31:03 -0500

    Challenge08-1

--- src/components/Son.js
+++ src/components/Son.js
@@ -1,13 +1,6 @@
-import React, { useMemo } from "react";
+import { memo } from "react";
 
-export const Son = ({ numero, increment }) => {
-  console.log('again reloaded...'); 
-
-  const MemoHook = useMemo(() => increment, [increment]);
-
-  return (
-    <button className='btn btn-primary mr-3' onClick={() => MemoHook(numero)}>
-      {numero}
-    </button>
-  );
-};
+export const Son = memo(({ numero, increment }) => {
+  console.log("Again reloaded...");
+  return <button onClick={() => increment(numero)}>{numero}</button>;
+});
