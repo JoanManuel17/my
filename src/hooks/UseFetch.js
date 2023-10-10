41fa5ee648046430180e07aa42c34a6da28b1325
JoanManuel17 <110619713+JoanManuel17@users.noreply.github.com>
2023-10-10 14:22:22 -0500

    Challenge08

--- src/hooks/UseFetch.js
+++ src/hooks/UseFetch.js
@@ -3,20 +3,28 @@ import { useEffect, useState } from "react";
 export const UseFetch = (url) => {
 
     const [state, setState] = useState({
-        data: null,
+        data: [],
         loading: true,
         error: null
     });
 
     const getFecth = async () => {
-        const api = await fetch(url);
-        const data = await api.json();
-
-        setState({
-            data: data,
-            loading: false,
-            error: null
-        });
+        try{
+            const api = await fetch(url);
+            const data = await api.json();
+    
+            setState({
+                data: data,
+                loading: false,
+                error: null
+            });
+        }catch(error){
+            setState({
+                data: [],
+                loading: false,
+                error: error.message
+            });
+        }
     }
 
     useEffect(() => {
