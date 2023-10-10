76a384cd8b3b166906025b9afa734ada2d75f534
JoanManuel17 <110619713+JoanManuel17@users.noreply.github.com>
2023-10-10 13:35:42 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -3,18 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ComponentApp } from "./ComponentApp";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import {MainApp}  from "./pages/MainApp";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <MainApp />
-  </BrowserRouter>
-
-  // <React.StrictMode>
-  //   <ComponentApp />
-  //   <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ComponentApp />
+    <App />
+  </React.StrictMode>
 );
